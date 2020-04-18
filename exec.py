93962409d7e8ffe0d600b86457c8093e182d23a8
Grzegorz Maciaszek <gmmaciaszek@gmail.com>
2020-04-18 18:37:37 +0200

    server can be configured with arguments.

--- exec.py
+++ exec.py
@@ -7,20 +7,29 @@ Created on Thu Nov 21 19:32:07 2019
 from TCP_Server import SelectorServer
 from collections import defaultdict
 import logging
+import argparse
 from dispatcher import Dispatcher
 
 
 def tree(): return defaultdict(tree)
 
 
-HOST = 'localhost'
-PORT = 21000
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s')
 
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', dest='host', type=str, default='localhost', help="defines server host")
+    parser.add_argument('--port', dest='port', type=int, default=21000, help="defines server port number")
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
+    args = parse()
+    logging.info(f'Server at {args.host}:{args.port}')
     logging.info('Battle.Server: Starting...')
     greetings = '-----<<<Battle.Server>>>-----\r\n---<<<CaptureTheNetwork>>>---\r\n'
-    server = SelectorServer(greetings, host=HOST, port=PORT)
+    server = SelectorServer(greetings, host=args.host, port=args.port)
     thread1 = Dispatcher(1, "Dispatcher", server)
     thread1.start()
     try:
