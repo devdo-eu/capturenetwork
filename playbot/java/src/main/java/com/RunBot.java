2d5df9feaa28eacc67de8e3b6069286eff4ee50f
Paweł P <pp.polakiewicz@gmail.com>
2020-04-29 23:00:45 +0200

    Review, pom and property fix

--- playbot/java/src/main/java/com/RunBot.java
+++ playbot/java/src/main/java/com/RunBot.java
@@ -2,9 +2,22 @@ package com;
 
 public class RunBot {
 
-    public static void main(String[] args){
+    /**
+     * Main method to create bot and start game.
+     * If no params pass then bot starts at localhost
+     * and default server port: 21000.
+     *
+     * @param args first argument is ip, second is port
+     */
+
+    public static void main(String[] args) {
         String host = "localhost";
         int port = 21000;
+
+        if (args.length == 2) {
+            host = args[0];
+            port = Integer.parseInt(args[1]);
+        }
 
         PlayBot playBot = new PlayBot(host, port);
         playBot.run();
