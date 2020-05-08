9ff01b199fab5f9de9e380ad8dc36269d825cb3a
Paweł P <pp.polakiewicz@gmail.com>
2020-05-08 21:55:30 +0200

    Change communication to new EOT char

--- playbot/java/src/main/java/com/communication/BotCommunicationImpl.java
+++ playbot/java/src/main/java/com/communication/BotCommunicationImpl.java
@@ -13,13 +13,16 @@ import java.util.Objects;
 @Log4j2
 public class BotCommunicationImpl implements BotCommunication {
 
+    public static final char EOT = '\u0004';
+    public static final String END_SIGN = "\n" + EOT + "\n";
+
     private PrintWriter output = null;
     private BufferedReader input = null;
 
     public BotCommunicationImpl(String host, int port) {
         try {
             Socket socket = new Socket(host, port);
-            output = new PrintWriter(socket.getOutputStream(), true);
+            output = new PrintWriter(socket.getOutputStream());
             input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         } catch (IOException e) {
             log.error(e.getMessage());
@@ -33,13 +36,19 @@ public class BotCommunicationImpl implements BotCommunication {
 
     @Override
     public void send(String data) {
-        output.println(data);
+        output.print(data + END_SIGN);
+        output.flush();
     }
 
     @Override
     public String read() throws IOException {
-        char[] msg = new char[1024];
-        int i = input.read(msg);
-        return String.valueOf(msg);
+        StringBuilder data = new StringBuilder();
+        String line;
+
+        while ((line = input.readLine()).indexOf(EOT) == -1) {
+            data.append(line);
+        }
+
+        return data.toString().trim();
     }
 }
